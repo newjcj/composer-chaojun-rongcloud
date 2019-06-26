903b7bf7ff1a5e6545ed9918bf633c64f83e9e0d
18666015093 <18666015093@163.com>
2019-06-26 11:09:27 +0800

    rm autoloader

--- RongCloud/Autoloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-function __autoload($class){
-    if(is_file(str_replace("\\", "/", RONGCLOUOD_ROOT.'../'."$class".'.'.'php'))){
-        require(str_replace("\\", "/", RONGCLOUOD_ROOT.'../'."$class".'.'.'php'));
-    }
-}
-spl_autoload_register("__autoload");
